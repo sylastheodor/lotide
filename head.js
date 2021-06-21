7bffb26965a6e8cbca3e03e070fd8fc495cb600a
Sylas Serné <its.sylas@gmail.com>
2021-06-21 23:06:17 +0000

    head

---
+++ head.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🌎🌹🌎 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🤯🥀🤯 Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+const head = function(array){
+  return array[0];
+};
+
+assertEqual(head([]), 5);
+assertEqual(head(["Lighthouse"]), "Lighthouse");
