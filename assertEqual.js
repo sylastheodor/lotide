3592f54283432cd1fec77540e52f9602facc5701
Sylas Serné <its.sylas@gmail.com>
2021-06-21 22:44:26 +0000

    I use the cutest pass/fail emojis

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('🌎🌹🌎 Assertion Passed: ' + actual + ' === ' + expected);
+  } else if (actual !== expected) {
+    console.log('🤯🥀🤯 Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+assertEqual("Scorsese", 'sohsese');
+assertEqual(1, 999);
+assertEqual(40, 40);
+assertEqual('cream', 'cream');
