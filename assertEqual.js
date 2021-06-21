9807a938f1b089dd4cf15757c1f383ff3fbc4628
Sylas Serné <its.sylas@gmail.com>
2021-06-21 23:03:01 +0000

    Template literalgit add assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('🌎🌹🌎 Assertion Passed: ' + actual + ' === ' + expected);
+    console.log(`🌎🌹🌎 Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log('🤯🥀🤯 Assertion Failed: ' + actual + ' !== ' + expected);
+    console.log(`🤯🥀🤯 Assertion Failed: ${actual} === ${expected}`);
   }
 };
 
