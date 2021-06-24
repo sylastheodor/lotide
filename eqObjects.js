9ae2a9864aa64196fb271c4a4eff5a6540d6185e
Sylas Serné <its.sylas@gmail.com>
2021-06-24 19:43:31 +0000

    refactored eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -12,31 +12,32 @@ const assertEqual = function(actual, expected) {
 //would it be better to make an array of keys from one object then compare it's length and
 //values to the second object??
 const eqObjects = function(object1, object2) {
-  let objKeys1 = [];
-  let length2 = 0;
-  let result = false;
-  for(keys1 in object1){
-    objKeys1.push(keys1);
+  if(Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
   }
+  let result;
   for(keys2 in object2){
-    length2 += 1;
     if (object1[keys2] !== object2[keys2]){
       result = false;
     } else {
       result = true;
     }
   }
-  if(objKeys1.length !== length2) {
-    result = false;
-  }
   return result;
 }
 
 
 
-const ab = {a:"1", b:"2", c:'the worst word', d: '2'};
-const ba = {b:"2", a:"1", c:'the worst word', d: '2'};
-assertEqual(eqObjects(ab, ba), true)
+// const ab = {a:"1", b:"2"};
+// const ba = {b:"2", a:"1",};
+// assertEqual(eqObjects(ab, ba), true)
 
-const abc = { a: "1", b: "2", c: "3"};
-assertEqual(eqObjects(ab, abc), false)
+// const abc = { a: "1", b: "2", c: "3"};
+// assertEqual(eqObjects(ab, abc), false)
+
+const cd = { c: "1", d: ["2", 3], e: 0 };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), false)
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false)
