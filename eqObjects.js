2c036e60630e17f9d152f596c2f46bddb61da7f1
Sylas Serné <its.sylas@gmail.com>
2021-06-24 20:04:15 +0000

    now she does arrays too!

--- eqObjects.js
+++ eqObjects.js
@@ -6,37 +6,38 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-//assertEqual is only for primitive objects
-//could use it to compare the values of an object property
-//could use it to LOOP thru the object properties and return a true if they match
-//would it be better to make an array of keys from one object then compare it's length and
-//values to the second object??
+const eqArrays = function (arr1, arr2){
+  for (let i = 0; i < arr1.length; i++){
+    if(arr1[i] !== arr2[i]){
+      return false
+    } 
+  }
+  return true 
+}
+
 const eqObjects = function(object1, object2) {
   if(Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
   let result;
   for(keys2 in object2){
-    if (object1[keys2] !== object2[keys2]){
+    if (Array.isArray(object1[keys2]) && Array.isArray(object2[keys2])){
+      if (eqArrays(object1[keys2], object2[keys2]) === false){
+        return false;
+      }
+    } else if (object1[keys2] !== object2[keys2]){
       result = false;
     } else {
       result = true;
     }
   }
   return result;
-}
+};
 
 
 
-// const ab = {a:"1", b:"2"};
-// const ba = {b:"2", a:"1",};
-// assertEqual(eqObjects(ab, ba), true)
-
-// const abc = { a: "1", b: "2", c: "3"};
-// assertEqual(eqObjects(ab, abc), false)
-
-const cd = { c: "1", d: ["2", 3], e: 0 };
-const dc = { d: ["2", 3], c: "1" };
+const cd = { c: "1", d: ["2", 3, 6, 8, 9]};
+const dc = { d: ["2", 3, 6, 8, 9], c: "1" };
 assertEqual(eqObjects(cd, dc), false)
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
