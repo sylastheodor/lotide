066d0f93b96fb7853109afc6f3556f53a16fec28
Sylas Serné <its.sylas@gmail.com>
2021-06-25 01:34:36 +0000

    cleaned up takeUntil

--- takeUntil.js
+++ takeUntil.js
@@ -1,20 +1,3 @@
-const eqArrays = function (arr1, arr2){
-  for (let i = 0; i < arr1.length; i++){
-    if(arr1[i] !== arr2[i]){
-      return false;
-    } 
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if(eqArrays(actual, expected) === true) {
-    console.log(`🌸😌🌸 Assertion Passed: ${actual} === ${expected}`);
-  } else if(eqArrays(actual, expected) === false) {
-    console.log(`🦠😵‍💫🦠 Assertion Failed: ${actual} === ${expected}`);
-  }
-};
-
 const takeUntil = function(array, callback) {
   results = []
   for (elm of array){
@@ -26,15 +9,3 @@ const takeUntil = function(array, callback) {
   }
   return results;
 };
-
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-
-const data3 = [2, 5, 7, 11, 3, 4, 6]
-assertArraysEqual(takeUntil(data3, x => x > 10), [2, 5, 7])
-
